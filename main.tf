ff5e49693f330460716a85c97af995f92ecfa420
shivakumar27342 <55353497+shivakumar27342@users.noreply.github.com>
2022-04-25 21:24:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,8 @@
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
 
-resource "null_resource" "upload_folder_content" {
-
- triggers = {
-
-   file_hashes = jsonencode({
-
-   for fn in fileset(var.folder_path, "**") :
-
-   fn => filesha256("${var.folder_path}/${fn}")
-
-   })
-
- }
-
- provisioner "local-exec" {
-
-   command = "gsutil -m rsync -r ${var.folder_path} gs://${var.gcs_bucket}/dag/${var.repo_name}"
-
- }
-
+  labels = {
+    foo = "bar"
+  }
 }
+
